e23d96bfe6e232bdc13ba97feabc0a01a6f5fc93
Runweztt <amarikwat@gmail>
2026-04-02 11:39:39 -0700

    Fix tasks 0 and 4

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""A script that fetches https://alu-intranet.hbtn.io/status using urllib."""
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
 import urllib.request
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen(url) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
