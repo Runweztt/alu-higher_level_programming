d7977f7299c6d4669d6b7a5ec212a0924318673e
Runweztt <amarikwat@gmail>
2026-04-02 11:47:42 -0700

    Fix task 0 direct URL fetch

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 Python script that fetches https://alu-intranet.hbtn.io/status
 """
 import requests
+
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
     if url.startswith('https://'):
