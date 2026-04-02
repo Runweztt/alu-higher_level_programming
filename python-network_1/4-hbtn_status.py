4f03f5e4bc5b4488669a6f6625f780e23422934d
Runweztt <amarikwat@gmail>
2026-04-02 11:51:32 -0700

    Fix task 4 use local server URL

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,12 +3,8 @@
 Python script that fetches https://alu-intranet.hbtn.io/status
 """
 import requests
-
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    res = requests.get('http://0.0.0.0:5050/status')
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
